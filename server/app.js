d86b7baa3c33ba9a3265fe3f7ef3330cd3afba80
thaitam <phithaitam@gmail.com>
2020-12-22 19:55:44 +0700

    routes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 // Require packages and set the port
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = require('./routes/routers.js');
 const port = 3002;
 const app = express();
 // Use Node.js body parsing middleware
@@ -8,12 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
-app.get('/', (request, response) => {
-    console.log(`URL: ${request.url}`);
-    response.send({
-        message: 'Node.js and Express REST API'}
-    );
-});
+routes(app);
 // Start the server
 const server = app.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
